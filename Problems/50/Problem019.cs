630967b087a72d89edac21072befdfce473a148b
Suyash <suyash.khandwe@gmail.com>
2016-03-09 17:01:35 -0800

    Problem 19
    Adds solution for problem 19 - count of Sundays. The solution was to
    loop thru all the years and months for 1st of the month and check if the
    day was a Sunday

---
+++ Problems/50/Problem019.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problems._50 {
+    public class Problem019 : IProblem {
+
+        private const int YY_START = 1901;
+        private const int YY_END = 2000;
+        private const int MM_START = 1;
+        private const int MM_END = 12;
+        private const int DD = 1;
+
+        public void ProblemSolver() {
+            var counter = 0;
+            for (var yy = YY_START; yy <= YY_END; yy++) {
+                for (var mm = MM_START; mm <= MM_END; mm++) {
+                    counter += (new DateTime(yy, mm, DD).DayOfWeek == DayOfWeek.Sunday) ? 1 : 0;
+                }
+            }
+            Console.WriteLine("Sundays on 1st of month - {0}", counter);
+        }
+    }
+}
--- ProjectEuler/MainEngine.cs
+++ ProjectEuler/MainEngine.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
 using Problems._100;
+using Problems._50;
 
 namespace ProjectEuler {
     internal static class MainEngine {
         public static void Main(String[] args) {
             var watch = Stopwatch.StartNew();
 
-            new Problem067().ProblemSolver();
+            new Problem019().ProblemSolver();
 
             watch.Stop();
             var ms = watch.ElapsedMilliseconds;
